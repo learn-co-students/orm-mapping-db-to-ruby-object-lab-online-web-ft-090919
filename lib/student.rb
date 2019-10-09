f5671af6724c099dfb8020fb7fc830afdd22a42a
DBuckley0126 <dannyboy.msn@hotmail.com>
2019-10-09 13:33:19 +0100

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,16 +3,41 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    new_song = self.new
+    new_song.id = row[0]
+    new_song.name = row[0]
+    new_song.grade = row[0]
+    new_song
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT * 
+      FROM students
+    SQL
+
+    DB[:conn].exucute(sql).map do |row|
+      self.new_from_db(row)
+    end
+
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+  sql = <<-SQL
+    SELECT * 
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+
+  DB[:conn].exucute(sql, name).map do |row|
+    self.new_from_db(row)
+  end
+
   end
   
   def save
