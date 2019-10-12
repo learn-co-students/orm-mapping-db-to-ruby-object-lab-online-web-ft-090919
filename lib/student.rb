5852a6fb189ec56c85b1337b8866f32022b8031a
Randel Jiao <randeljiao@gmail.com>
2019-10-12 20:10:13 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,6 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    
     student = self.new
     student.id = row[0]
     student.name = row[1]
@@ -14,8 +13,7 @@ class Student
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
-    
-     sql = <<-SQL
+    sql = <<-SQL
       SELECT *
       FROM students
     SQL
@@ -28,7 +26,7 @@ class Student
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
-     sql = <<-SQL
+    sql = <<-SQL
       SELECT *
       FROM students
       WHERE name = ?
@@ -39,8 +37,8 @@ class Student
       self.new_from_db(row)
     end.first
   end
-  
-   def save
+
+  def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
